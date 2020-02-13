51677104ded6b00ba0eab0d34b1a155ba1442083
DrZin <pasawee.dev@gmail.com>
2020-02-13 09:39:34 +0700

    create post modeel

---
+++ model/post.go
@@ -0,0 +1,12 @@
+package model
+
+//Post model
+type Post struct {
+	ID          int    `json:"id" db:"id"`
+	AccountID   int    `json:"account_id" db:"account_id"`
+	Topic       string `json:"topic" db:"topic"`
+	Location    string `json:"location" db:"location"`
+	Description string `json:"description" db:"description"`
+	UpdatedAt   string `json:"updated_at" db:"updated_at"`
+	CreatedAt   string `json:"created_at" db:"created_at"`
+}
