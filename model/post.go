2c14cea2a7f5f03dba7c0704f5e93bcb930641a0
DrZIn-dev <quick.car6020@fastmail.com>
2020-02-13 09:47:26 +0700

    init CreatePost model

--- model/post.go
+++ model/post.go
@@ -10,3 +10,13 @@ type Post struct {
 	UpdatedAt   string `json:"updated_at" db:"updated_at"`
 	CreatedAt   string `json:"created_at" db:"created_at"`
 }
+
+//CreatePost model
+type CreatePost struct {
+	AccountID   int    `json:"account_id" from:"account_id" query:"account_id"`
+	Topic       string `json:"topic" from:"topic" query:"topic"`
+	Location    string `json:"location" from:"location" query:"location"`
+	Description string `json:"description" from:"description" query:"description"`
+	UpdatedAt   string `json:"updated_at" from:"updated_at" query:"updated_at"`
+	CreatedAt   string `json:"created_at" from:"created_at" query:"created_at"`
+}
