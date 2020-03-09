a6f10db62a94f728f9d722b353ef5f74a64df7c2
Tantatorn <timefear007@gmail.com>
2020-03-09 21:07:46 +0700

    add profile image

--- model/account.go
+++ model/account.go
@@ -8,4 +8,5 @@ type Account struct {
 	FirstName  string `json:"first_name" db:"first_name"`
 	LastName   string `json:"last_name" db:"last_name"`
 	CoinAmount int    `json:"coin_amount" db:"coin_amount"`
+	ProfileIMGPath string `json:"profile_img_path,omitempty" db:"profile_img_path,omitempty"`
 }
