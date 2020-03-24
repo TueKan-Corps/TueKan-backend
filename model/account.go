520401c3283cef8afff0f4fd49ce88c0fabbbff9
Tantatorn <timefear007@gmail.com>
2020-03-24 23:02:17 +0700

    add contact info to account

--- model/account.go
+++ model/account.go
@@ -1,12 +1,20 @@
 package model
 
+// Contact type
+type Contact struct {
+	ID   int
+	Name string
+	Link string
+}
+
 // Account model
 type Account struct {
-	ID         int    `json:"id" db:"id"`
-	Username   string `json:"username" db:"username"`
-	Password   string `json:"password" db:"password"`
-	FirstName  string `json:"first_name" db:"first_name"`
-	LastName   string `json:"last_name" db:"last_name"`
-	CoinAmount int    `json:"coin_amount" db:"coin_amount"`
-	ProfileIMGPath string `json:"profile_img_path,omitempty" db:"profile_img_path,omitempty"`
+	ID             int        `json:"id" db:"id"`
+	Username       string     `json:"username" db:"username"`
+	Password       string     `json:"password" db:"password"`
+	FirstName      string     `json:"first_name" db:"first_name"`
+	LastName       string     `json:"last_name" db:"last_name"`
+	CoinAmount     int        `json:"coin_amount" db:"coin_amount"`
+	ProfileIMGPath string     `json:"profile_img_path,omitempty" db:"profile_img_path,omitempty"`
+	Contact        [5]Contact `json:"contact" db:"contact"`
 }
