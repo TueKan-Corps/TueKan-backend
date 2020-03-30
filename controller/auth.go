196d9e122aba71c2112c18c7b773633bc7f3d659
Tantatorn <timefear007@gmail.com>
2020-03-30 20:03:08 +0700

    add model Session for manage login/logout activities

--- controller/auth.go
+++ controller/auth.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"TueKan-backend/model"
 	"github.com/labstack/echo"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func (a *AccountController) Login(c echo.Context) error {
@@ -25,14 +28,36 @@ func (a *AccountController) Login(c echo.Context) error {
 	}
 
 	// return these things as a key
-	key := new(model.Key)
+	session := new(model.Session)
 
 	queryString = "SELECT id FROM  account WHERE username=$1"
 	row = a.DB.QueryRow(queryString, inputUsername)
-	err = row.Scan(&key.AccountID)
+	err = row.Scan(&session.AccountID)
 	if err != nil {
 		return err
 	}
-	key.SecretKey = "very_secret"
-	return c.JSON(http.StatusOK, key)
+
+	// Generate random token
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
+		"abcdefghijklmnopqrstuvwxyzåäö" +
+		"0123456789")
+	length := 100
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	str := b.String()
+	session.Token = str
+
+	// Record a token into Session
+	queryString = "INSERT INTO session(account_id, token) VALUES ($1,$2) RETURNING id"
+
+	row = a.DB.QueryRow(queryString, session.AccountID, session.Token)
+	err = row.Scan(&session.ID)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, session)
 }
--- model/key.go
+++
@@ -1,7 +0,0 @@
-package model
-
-// Key model
-type Key struct {
-	AccountID int    `json:"account_id"`
-	SecretKey string `json:"secret_key"`
-}
