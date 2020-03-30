3d76b7d2ec852580ab92ff8752c77ac309393792
Tantatorn <timefear007@gmail.com>
2020-03-30 20:39:12 +0700

    add get all sessions

---
+++ routes/session.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"TueKan-backend/controller"
+	"TueKan-backend/db"
+	"github.com/labstack/echo"
+)
+
+//Session routes for Post API
+func Session(app *echo.Echo) {
+	sessionController := controller.NewSessionController(db.DB)
+
+	sessionRoute := app.Group("/session")
+	sessionRoute.GET("/", sessionController.GetAll)
+}
