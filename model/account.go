4c46def6ba993708901a7bef96cc1a8fb529e3d4
DrZIn-dev <quick.car6020@fastmail.com>
2020-04-05 13:31:49 +0700

    Update Query string sort by startat desc

--- model/account.go
+++ model/account.go
@@ -43,3 +43,11 @@ func (a *Account) ComparePassword(plainPassword string) error {
 
 	return nil
 }
+
+type UpdateAccount struct {
+	ID          int        `json:"id"`
+	FirstName   string     `json:"first_name"`
+	LastName    string     `json:"last_name"`
+	Description string     `json:"description"`
+	Contact     [5]Contact `json:"contact"`
+}
